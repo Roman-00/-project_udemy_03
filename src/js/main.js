19846d97b5e8c91d7204eda2a5ec91590509fcda
Roman00 <rvasin00@gmail.com>
2020-07-04 09:24:56 +0600

    project: done

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,12 @@ import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
 import Form from './modules/forms';
+import ShowInfo from './modules/showinfo';
+import Download from './modules/download';
+
 
 window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
     const slider = new MainSlider({btns: '.next', container: '.page'});
     slider.render();
 
@@ -38,9 +42,13 @@ window.addEventListener('DOMContentLoaded', () => {
     });
     feedSlider.init();
 
-    const player = new VideoPlayer('.showup .play', '.overlay');
-    player.init();
+    new VideoPlayer('.showup .play', '.overlay').init();
+    new VideoPlayer('.module__video-item .play', '.overlay').init();
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
     new Form('.form').init();
+
+    new ShowInfo('.plus__content').init();
+
+    new Download('.download').init();
 });
